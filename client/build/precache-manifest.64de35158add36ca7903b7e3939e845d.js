7c098fdf76df92926431733dc5da4c52f6a8fb77
Adam Romano <adamromano@Adams-iMac.lan>
2022-01-02 16:36:29 -0600

    aaa

---
+++ client/build/precache-manifest.64de35158add36ca7903b7e3939e845d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a76ad9e19585684182051eca8d71af8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a7528952f8e8e4d9d780",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "41cd2f7ab44e2479e6af",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "a7528952f8e8e4d9d780",
+    "url": "/static/js/2.f6469185.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/static/js/2.f6469185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cd2f7ab44e2479e6af",
+    "url": "/static/js/main.3a5b5d8c.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
